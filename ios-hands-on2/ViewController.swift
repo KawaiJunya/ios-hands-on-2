01b7a53bb806b44a7bef309da94e563eb7fdd407
Junya Kawai <junya.kawai@nextbeat.net>
2020-01-22 20:08:42 +0900

    Create new project

---
+++ ios-hands-on2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-hands-on2
+//
+//  Created by junya.kawai on 2020/01/22.
+//  Copyright © 2020 nextbeat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
